b2c3427bcbea5798a0f24c9f4405709ce1f64316
Alick2017 <445760068@qq.com>
2017-11-12 17:59:39 +0800

    c++ test

---
+++ Constant/Constant.cpp
@@ -0,0 +1,37 @@
+// Constant.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <iostream>;
+using namespace std;
+ 
+/*#define LENGTH 10   
+ *#define WIDTH  5
+ *#define NEWLINE '\n'
+ 
+ *int main()
+ *{
+ 
+ * int area;  
+   
+ * area = LENGTH * WIDTH;
+ *  cout << area;
+ *  cout << NEWLINE;
+ * return 0;
+}*/
+
+
+int main()
+{
+   const int  LENGTH = 10;
+   const int  WIDTH  = 5;
+   const char NEWLINE = '\n';
+   int area;  
+   
+   area = LENGTH * WIDTH;
+   cout << area;
+   cout << NEWLINE;
+   return 0;
+}
+
